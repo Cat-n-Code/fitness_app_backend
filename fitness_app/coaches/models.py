74175735e86e895c18ddea8c6edebf83c7a336ff
TheAtlac <sash.nemov@yandex.ru>
2024-06-03 21:18:40 +0400

    fixed models relationship

--- fitness_app/coaches/models.py
+++ fitness_app/coaches/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from fitness_app.core.db_manager import Base
 
@@ -10,3 +10,4 @@ class Coach(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    user: Mapped["User"] = relationship(back_populates="coach_info")
--- fitness_app/users/models.py
+++ fitness_app/users/models.py
@@ -1,7 +1,7 @@
 from datetime import date
 
 from sqlalchemy import UniqueConstraint
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from fitness_app.core.db_manager import Base
 from fitness_app.users.schemas import Sex
@@ -16,5 +16,7 @@ class User(Base):
     sex: Mapped[Sex] = mapped_column(nullable=True)
     birth_date: Mapped[date] = mapped_column(nullable=True)
     password_hash: Mapped[str] = mapped_column(nullable=False)
+    customer_info: Mapped["Customer"] = relationship("Customer", back_populates="user")
+    coach_info: Mapped["Coach"] = relationship("Coach", back_populates="user")
 
     __table_args__ = (UniqueConstraint(email),)
