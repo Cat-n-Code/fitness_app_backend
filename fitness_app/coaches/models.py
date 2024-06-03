a52da7b016f9aec05ef265489bee0448c82e349e
TheAtlac <sash.nemov@yandex.ru>
2024-06-03 18:42:45 +0400

    coach and customer empty models

---
+++ fitness_app/coaches/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from fitness_app.core.db_manager import Base
+from fitness_app.users.models import User
+
+
+class Coach(Base):
+    __tablename__ = "coaches"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    user: Mapped["User"] = relationship(back_populates="coach_info")
