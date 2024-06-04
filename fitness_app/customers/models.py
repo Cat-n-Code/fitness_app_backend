7b73557315cbcc43b20c9a171166da728107faf6
TheAtlac <sash.nemov@yandex.ru>
2024-06-04 20:34:30 +0400

    models fixes

--- fitness_app/customers/models.py
+++ fitness_app/customers/models.py
@@ -1,7 +1,13 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from fitness_app.core.db_manager import Base
+
+if TYPE_CHECKING:
+    from fitness_app.coaches.models import Coach
+    from fitness_app.users.models import User
 
 
 class Customer(Base):
@@ -11,3 +17,6 @@ class Customer(Base):
     name: Mapped[str] = mapped_column(nullable=False)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     user: Mapped["User"] = relationship(back_populates="customer_info")
+
+    coach_id: Mapped[int] = mapped_column(ForeignKey("coaches.id"), nullable=False)
+    coach: Mapped["Coach"] = relationship(back_populates="customers")
--- fitness_app/users/models.py
+++ fitness_app/users/models.py
@@ -3,7 +3,9 @@ from datetime import date
 from sqlalchemy import UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+from fitness_app.coaches.models import Coach
 from fitness_app.core.db_manager import Base
+from fitness_app.customers.models import Customer
 from fitness_app.users.schemas import Sex
 
 
@@ -16,6 +18,7 @@ class User(Base):
     sex: Mapped[Sex] = mapped_column(nullable=True)
     birth_date: Mapped[date] = mapped_column(nullable=True)
     password_hash: Mapped[str] = mapped_column(nullable=False)
+
     customer_info: Mapped["Customer"] = relationship("Customer", back_populates="user")
     coach_info: Mapped["Coach"] = relationship("Coach", back_populates="user")
 
