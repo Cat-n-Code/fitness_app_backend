e31021556d8c26b0ec19b1f4d4c58c393e6dfd03
TheAtlac <sash.nemov@yandex.ru>
2024-06-10 02:11:45 +0400

    add chat deleting (in chat service)

--- fitness_app/chats/services.py
+++ fitness_app/chats/services.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from fitness_app.chats.models import Chat
 from fitness_app.chats.repositories import ChatRepository
-from fitness_app.chats.schemas import ChatCreateSchema, ChatType
+from fitness_app.chats.schemas import ChatCreateSchema, ChatSchema, ChatType
 from fitness_app.core.exceptions import (
     BadRequestException,
     EntityNotFoundException,
@@ -88,3 +88,12 @@ class ChatService:
                 UserSchema.model_validate(user) for user in chat_dict["users"]
             ]
         return PageSchema(total_items_count=total_chats_count, items=chat_dicts)
+
+    async def delete_by_id(
+        self, session: AsyncSession, user: User, chat_id: int
+    ) -> ChatSchema:
+        chat = await self.get_by_chat_id(session, user, chat_id)
+        schema = ChatSchema(**chat.__dict__)
+
+        await self._chat_repository.delete(session, chat)
+        return schema
